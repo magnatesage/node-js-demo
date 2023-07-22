12368a824e82395845bb4ad7f4e5937e7b0dd095
magnatesage <45814720+magnatesage@users.noreply.github.com>
2023-07-22 18:35:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const ip = require("ip");
 
 app.get('/', function (req, res) {
-  res.send(`Hello World : ${ip.address()} | nevil test ec2 test`)
+  res.send(`Hello World : ${ip.address()} | nevil test ec2 test with anjali`)
 })
 
 app.listen(5555)
