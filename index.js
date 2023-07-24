75fca11eeef171bf274a77492b5ee77a7c2279ce
nevil-dev <neviltest@gmail.com>
2023-07-24 08:45:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const ip = require("ip");
 
 app.get('/', function (req, res) {
-  res.send(`Hello World : ${ip.address()} | CICD RUN`)
+  res.send(`Hello World : ${ip.address()} | CICD RUN | ${new Date()}`)
 })
 
 app.listen(5555)
