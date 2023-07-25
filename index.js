de8d1406df473de0a78703221f80c1744e40d2b7
nevil-dev <neviltest@gmail.com>
2023-07-25 18:29:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const ip = require("ip");
 
 app.get('/', function (req, res) {
-  res.send(`Hello World : ${ip.address()} | Check Service Node js Project`)
+  res.send(`Hello World : ${ip.address()} | Check Service Node js Project 1`)
 })
 
 app.listen(5555)
